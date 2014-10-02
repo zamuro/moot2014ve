e463a9a5c6682ccae57da7310f175658191aac2d
Manuel Lucena <mlucenap@venet.com.ve>
2014-10-02 10:14:13 -0430

    validación del número de voucher

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -1,6 +1,8 @@
 class Pago < ActiveRecord::Base
 	validates_presence_of :usuario_id, :monto, :voucher
 	validates_uniqueness_of :voucher
+	validates_length_of :voucher, :minimum => 7
+	validates_length_of :voucher, :maximum => 12
 	belongs_to :usuario
 	belongs_to :datos_registro
 	belongs_to :banco
