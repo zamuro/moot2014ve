cfed8efaedd578af6ec5947e25399d2ff4e29476
Manuel Lucena <mlucenap@venet.com.ve>
2014-10-09 13:54:29 -0430

    Validaciones de pagos

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -1,8 +1,9 @@
 class Pago < ActiveRecord::Base
 	validates_presence_of :usuario_id, :monto, :voucher
 	validates_uniqueness_of :voucher
-	validates_length_of :voucher, :minimum => 7
-	validates_length_of :voucher, :maximum => 12
+	validates :monto, numericality: { :greater_than => 100.00 }
+	validates_length_of :voucher, :minimum => 12
+	validates_length_of :voucher, :maximum => 15
 	belongs_to :usuario
 	belongs_to :datos_registro
 	belongs_to :banco
