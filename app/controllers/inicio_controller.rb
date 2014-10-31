e4068c22dc23a6e905621cf6d57653b054df5a28
Manuel Lucena <mlucenap@venet.com.ve>
2014-10-30 22:48:15 -0430

    posición consolidada

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,5 +1,12 @@
 class InicioController < ApplicationController
   def index
-  	
+  end
+
+  def consolidado
+  	@registros = DatosRegistro.all
+  	@fichas = FichasMedica.all
+  	@datos_usuarios = DatosUsuario.all
+  	@usuarios = Usuario.all
+  	@pago = Pago.all
   end
 end
