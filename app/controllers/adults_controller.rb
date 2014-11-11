9e6990e058c9f6c75e630c518de165c7a20bf425
Manuel Lucena <mlucenap@venet.com.ve>
2014-11-11 16:42:01 -0430

    Añadir parámetros en controlador de adultos... oops!

--- app/controllers/adults_controller.rb
+++ app/controllers/adults_controller.rb
@@ -5,4 +5,30 @@ class AdultsController < ApplicationController
 
   def index
   end
+
+  def create
+    @adult = Adult.new(adult_params)
+    
+    respond_to do |format|
+      if @adult.save
+        format.html { redirect_to "/inicio/success" }        
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @adult.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_adult
+      @adult = Adult.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def adult_params
+      params.require(:adult).permit(:nombre, :ci, :fechaNac, :region_id, :distrito_id)
+    end
+
 end
